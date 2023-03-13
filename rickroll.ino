00946ee9672e9c8dce3f1302e9a7d4272649b617
rielara <78863735+rielara@users.noreply.github.com>
2023-03-13 18:06:35 +0000

    Update rickroll.ino

--- rickroll.ino
+++ rickroll.ino
@@ -9,7 +9,7 @@ DigiKeyboard.sendKeyStroke(0);
 // Simulate keyboard inputs to open a new tab and go to a URL
 DigiKeyboard.sendKeyStroke(KEY_R , MOD_GUI_LEFT); // Press Windows key + r to open run box
 DigiKeyboard.delay(1000); // Wait for the search box to open
-DigiKeyboard.print("chrome.exe https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"); // Type the name of the browser you want to use
+DigiKeyboard.print("chrome.exe https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"); // Type the name of the browser you want to use + utl
 DigiKeyboard.sendKeyPress(KEY_ENTER); // Press Enter to open the browser
 DigiKeyboard.delay(2000); // Wait for the browser to open
 }
