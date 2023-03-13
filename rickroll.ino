a1bdc6fae48c6470e86c4db29fd3e22d235fb483
rielara <78863735+rielara@users.noreply.github.com>
2023-03-13 14:40:30 +0000

    Add files via upload

---
+++ rickroll.ino
@@ -0,0 +1,21 @@
+
+#include "DigiKeyboard.h"
+
+void setup() {
+// this is generally not necessary but with some older systems it seems to
+// prevent missing the first character after a delay:
+DigiKeyboard.sendKeyStroke(0);
+
+// Simulate keyboard inputs to open a new tab and go to a URL
+DigiKeyboard.sendKeyStroke(KEY_R , MOD_GUI_LEFT); // Press Windows key + r to open run box
+DigiKeyboard.delay(1000); // Wait for the search box to open
+DigiKeyboard.print("chrome.exe https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"); // Type the name of the browser you want to use
+DigiKeyboard.sendKeyPress(KEY_ENTER); // Press Enter to open the browser
+DigiKeyboard.delay(2000); // Wait for the browser to open
+}
+
+void loop() {
+
+
+}
+
